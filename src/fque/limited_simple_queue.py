c3b931e224543789d957ef51a82694b41a298fa1
AndroidLollipop <obfuscated00143241131643657478@gmail.com>
2022-08-12 19:32:44 +0800

    Update limited_simple_queue.py

--- src/fque/limited_simple_queue.py
+++ src/fque/limited_simple_queue.py
@@ -7,7 +7,7 @@ class LimitedSimpleQueue(queue.SimpleQueue):
         super().__init__()
         self.__maxsize = maxsize if maxsize > 0 else math.inf
 
-    def put(self, item, block=True, timeout=None):
+    def put(self, item):
         if self.qsize() > self.__maxsize:
             raise queue.Full
-        return super().put(item, block, timeout)
+        return super().put(item)
